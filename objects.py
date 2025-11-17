120071ba8e4f9bcfc0f8880bdb628e23459b3ec7
Pierre <pierre.laine07@gmail.com>
2025-11-17 12:18:51 +0100

    Add class

--- objects.py
+++ objects.py
@@ -11,3 +11,22 @@ class Calculatrice:
     
     def division(x, y):
         return x / y
+
+class Statistiques:
+
+    def moyenne(liste):
+        return sum(liste) / len(liste)
+    
+    def mediane(liste):
+        sorted_list = sorted(liste)
+        n = len(sorted_list)
+        mid = n // 2
+        if n % 2 == 0:
+            return (sorted_list[mid - 1] + sorted_list[mid]) / 2
+        else:
+            return sorted_list[mid]
+    
+    def variance(liste):
+        mean = Statistiques.moyenne(liste)
+        return sum((x - mean) ** 2 for x in liste) / len(liste)
+  
