89f6edb8e15a4d2302860afeda7819b8d54cd41c
Cocolegeek <151200331+Cocolegeek@users.noreply.github.com>
2025-11-17 11:59:00 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -11,3 +11,16 @@ class Calculatrice:
     
     def division(x, y):
         return x / y
+
+class DistributeurDeBonbons:
+    def __init__(self):
+        self.nb_bonbons = 10  # On commence avec 10 bonbons
+
+    def donner_bonbon(self):
+        if self.nb_bonbons > 0:
+            self.nb_bonbons -= 1
+            print("🍬 Voici un bonbon !")
+        else:
+            print("❌ Zut, il n'y a plus de bonbons !")
+          
+distributeur.donner_bonbon()  # Affiche "🍬 Voici un bonbon !"
