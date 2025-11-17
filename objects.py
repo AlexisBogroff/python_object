7d85cffd1e6d5000bf33b5a12372d173b824b8c9
carolinahermes-ai <carolina.hermes@qobuz.com>
2025-11-17 12:07:26 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -11,3 +11,15 @@ class Calculatrice:
     
     def division(x, y):
         return x / y
+
+
+class User:
+    def dados(self):
+        self.login = 
+        self.email = carolina@gmail.com
+        self.telephone = 0796129384
+        
+    def present(self)
+    print (f"my user is {self.login}, f" and my e-mail is {self.email})
+
+user_1 = User()
