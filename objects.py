adeded1f1ec8c2268ba3905e49bfa0b3cc67e549
OMÉGA HERITIANA <omigirihome@gmail.com>
2025-11-17 12:21:16 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -11,3 +11,18 @@ class Calculatrice:
     
     def division(x, y):
         return x / y
+
+class Conges:
+    
+    def __init__(self):
+
+     def jours_conges(self, mois: int): 
+        return 2,5 * mois
+
+    def afficher_conges(self, mois: int):
+        jours = self.jours_conges(mois)
+        print(f"Ce mois, tu as {jours_conges} de congés disponibles !")
+
+
+
+    
