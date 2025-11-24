abb5bdedd7e597ad6b2eacb78f1e8ccda1de6c24
atandabany <tandabany.adrien@hotmail.fr>
2025-11-24 11:24:11 +0100

    feat(project): add exercices

---
+++ exercices.py
@@ -0,0 +1,75 @@
+# exercice 1
+# def entree_autorisee(age):
+#     if age >= 18:
+#         return True
+#     else:
+#         return False
+
+# print(entree_autorisee(20))
+
+
+# exercice 2
+# # x = "avbrpeobnrg"
+# # for i in x:
+# #     # print(i)
+
+
+# # exercice 3
+# age = 18
+# print(f"Vous avez {age} ans.")
+
+
+# exercice 4
+# tortue = "une tortue"
+# saute = "saute sur"
+# chat = "un chat"
+
+# print(f"{tortue} {saute} {chat}.")
+
+
+# exercice 5
+# a = 5
+# b = 10
+# temp = None
+
+# print("Before swapping:")
+# print("a :", a)
+# print("b :", b)
+
+# temp = a 
+# a = b
+# b = temp
+
+# print("After swapping:")
+# print("a :", a)
+# print("b :", b)
+
+
+# exercice 6
+# dico = {
+#     "nom": "Marie",
+#     "âge": 27,
+#     "fruit": "pomme"
+# }
+
+# nom = dico["nom"]
+# age = dico["âge"]
+# fruit = dico["fruit"]
+
+# print(f"Une personne qui s'appelle {nom} et qui a {age} ans et mange des {fruit}s.")
+
+
+# exercice 7
+# text = "il fait beau"
+# l = []
+# mots = text.split()
+# for mot in mots:
+#     l.append(mot)
+# print(l)
+
+
+# exercice 8
+# l = ["a", "b", "c", "d"]
+
+# for key, value in enumerate(l):
+#     print(f"{value}{key}")
